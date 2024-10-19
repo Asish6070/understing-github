0030d49f20c58c0b38e302d69f6972568f1d019b
Asish6070 <asishkumar6070@gmail.com>
2024-10-19 19:24:59 +0530

    one file

---
+++ Addition.java
@@ -0,0 +1,14 @@
+package Demo;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int n1=10,n2=20;
+		int sum=n1+n2;
+		
+		System.out.println("'"+n1+"'"+"+"+"'"+n2+"' = "+"'"+sum+"'");
+		
+
+	}
+
+}
